6a0159a7ede3d6d0887a163049a864937ad8ba31
Gabriel Falcão <gabrielteratos@gmail.com>
2025-08-27 04:41:46 +0000

    build-script

--- build-pages.sh
+++ build-pages.sh
@@ -6,4 +6,29 @@ git_path="$(git rev-parse --show-toplevel)"
 source_path="${git_path}/web"
 
 
-cd ${source_path} && npm install && npm run build
+declare -a changes=( $(git status --porcelain | awk '{ print $NF }') );
+
+if [ ${#changes[@]} -gt 0 ]; then
+    1>&2 echo "commit pending changes before generating gh-pages: ${changes[@]}"
+    exit 101
+fi
+
+branch=$(git branch | grep '^[[:space:]]*[*]' | head -1 | awk '{ print $NF }')
+
+if [ "${branch}" != "gh-pages" ]; then
+    git switch -C "gh-pages"
+fi
+
+(cd ${source_path} && npm install && npm run build)
+if [ -e "${git_path}/index.html"]; then
+    rm -f ${git_path}/index.html
+fi
+if [ -e "${git_path}/assets"]; then
+    rm -rf ${git_path}/assets
+fi
+
+mv -fv ${source_path}/dist/* .
+git add -f assets index.html
+git commit -am "gh-pages"
+git push -u origin gh-pages
+git switch "${branch}"
